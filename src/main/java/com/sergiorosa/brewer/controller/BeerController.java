ab082d03c4c6ae86555f6fb449d9eff628bba264
Sergio Rosa <scarlosjazz@gmail.com>
2019-12-30 10:32:52 -0500

    Spring Configuration

--- src/main/java/com/sergiorosa/brewer/controller/BeerController.java
+++ src/main/java/com/sergiorosa/brewer/controller/BeerController.java
@@ -1,8 +1,15 @@
 package com.sergiorosa.brewer.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class BeerController {
+public class BeerController 
+{
+	@RequestMapping("/beer/novo")
+	public String novo()
+	{
+		return "beer/BeerRegister";
+	}
 
 }
